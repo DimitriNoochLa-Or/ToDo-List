5b0d13dda57a6efed66de50b3e6d7fba06057ec6
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-04-14 12:00:47 -0700

    created connect.php .  made it connect to mysqli if it doesn't connect it will cause an error

---
+++ includes/connect.php
@@ -0,0 +1,16 @@
+<?php 
+//connection to the mysql server
+//why is this important? why does local host have to be first?
+//the host name localhost has a special meaning. it is bound to the use of unix domain sockets. 
+//it is not possible to opan a tcp/ip connection using the hostname localhost
+//$mysqli->connect error then we want it to die and have this message
+$mysqli = new mysqli('localhost' , 'root' , 'root' , 'todo');//task is the name of the database
+if ($mysqli-> connect_errno) {
+	die('Connect Error(' . $mysqli->connect_errno . ')' 
+	. $mysqli->connect_error);
+}else{
+	echo "Connection Made";
+
+}
+$mysqli->close();
+?>
