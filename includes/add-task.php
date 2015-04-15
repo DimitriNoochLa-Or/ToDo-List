431671f255c2c13b1e989e0992f7d2116bd7f7aa
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-04-15 12:20:25 -0700

    finished video 11 added add-task.php coded and commented and added $mysqli variables

---
+++ includes/add-task.php
@@ -0,0 +1,17 @@
+<?php 
+	$task = strip_tags($_POST['task']);
+	$date = date('Y-m-d');
+	$time = date('H:i:s');
+
+	include('connect.php');// connecting to the connect.php
+
+
+$mysqli = new msqli("localhost" , "root", "root", 'tasks');
+$mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
+//the msqli querys all the things above into the data base
+//then $query queries the task that we have to put it in there
+
+$query = "SELECT * FROM tasks WHERE task='$task'  and date='$date' and time='$time'";
+
+
+ ?>
