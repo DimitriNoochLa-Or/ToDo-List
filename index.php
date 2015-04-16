f6ae40b55e7125ae170e134b08d17b384eef13d7
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-04-16 12:13:37 -0700

    finished video 16

--- index.php
+++ index.php
@@ -20,10 +20,11 @@
 			 						$task_id = $row['id'];
 			 						$task_name = $row['task'];
 
-
-			 					 echo "<li>
-			 					 <span>'.$task_name'
-			 					 ";
+			 						//need single qoutes becuz we are using double inside
+			 					 echo '<li>
+			 					 <span>'.$task_name. '</span>
+			 					 <img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+								 </li>';
 			 					}
 			 				}
 			 			}
