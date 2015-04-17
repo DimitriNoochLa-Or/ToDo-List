6a818cfe44f6ee28685f7badc0b00dfd360d6e13
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-04-17 11:51:30 -0700

    finished video 19

--- includes/delete-task.php
+++ includes/delete-task.php
@@ -4,7 +4,7 @@ require('connect.php');
 $mysqli = new mysqli('localhost' , 'root' , 'root' , 'todo');
 
 if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
-	# code...
+	
 }
 
 ?>
--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
 		<title> Simple To-Do List </title>
 		<link rel="stylesheet" type="text/css" href="css/main.css">	
-		<link rel="stylesheet" type="text/css" href="css/normalize.css">	
-		<link rel="stylesheet" type="text/css" href="css/reset.css">	
+	
 
 
 </head>	
@@ -36,7 +35,7 @@
 			 		</ul>
 			</div>
 		<form class="add-new-task" autocomplete="off">
-			<input type="text" name="new-task" placeholder="Add new item..."/>
+			<input class="tasklength" type="text" name="new-task" placeholder="Add new item..."/>
 		</form>
 		</div>
 </body>
@@ -53,7 +52,7 @@
 				if (new_task != ''){
 					$.post('includes/add-task.php', {task: new_task}, function(data) {
 						$('add-new-task input[name=new-task]').val();
-							$(data).appendTo('task-list ul').hide().fadein();
+							$(data).appendTo('.task-list ul').hide().fadein();
 					});
 				}
 				return false;
